9076a0a0e8a7954759dee4d18985034a9d6701b4
Abhinay1227 <kabhinaytej321@gmail.com>
2024-12-07 12:36:03 +0530

    Add files via upload

---
+++ jfsd-endsem-lab/jfsd-endsem-lab/src/test/java/com/klef/jfsd_endsem_lab/JfsdEndsemLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd_endsem_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfsdEndsemLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
